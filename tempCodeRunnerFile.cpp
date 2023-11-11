a8f16b7c36d68f880aa41893bf3b26a41fbaf55d
Shekhar Dase <shekhardase@gmail.com>
2023-11-11 17:33:44 +0530

    day 1 of recursion

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,15 @@
-   // for (int i = 0; i < n; i++)
-    // {
-    //     cout<<arr[i]<<" ";
-    // }
+void print(int n)
+// {
+
+//     // this is tail recursive
+
+//     if (n == 0)
+//     {
+//         return;
+//     }
+
+//     cout << n << endl;
+
+//     // recursive relation
+//     print(n - 1);
+// }
