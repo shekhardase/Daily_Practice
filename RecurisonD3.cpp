6233a3c1a4260ff1638bb3bfc5fc9d98b9e978d6
Shekhar Dase <shekhardase@gmail.com>
2023-11-18 19:25:18 +0530

    String Palindrome using recursion

--- RecurisonD3.cpp
+++ RecurisonD3.cpp
@@ -1,28 +1,31 @@
 // Lecture focuses on question solving
 //  Q1 . Reverse the string using recursion
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-void reverse(string str, int end)
-{
-    // end = str.size() - 1;
+// void reverse(string str, int end)
+// {
+//     // end = str.size() - 1;
 
-    if (end < 0)
-    {
-        return;
-    }
-    cout << str[end] << " ";
+//     if (end < 0)
+//     {
+//         return;
+//     }
+//     cout << str[end] << " ";
 
-    reverse(str, end - 1);
-}
+//     reverse(str, end - 1);
+// }
 
-int main()
-{
-    string str;
-    cin >> str;
+// int main()
+// {
+//     string str;
+//     cin >> str;
 
-    reverse(str, str.size() - 1);
-}
+//     reverse(str, str.size() - 1);
+// }
+
+// reverse string using recursion but with swap function
+
 // #include <iostream>
 // using namespace std;
 
@@ -58,3 +61,49 @@ int main()
 
 //     return 0;
 // }
+
+// String palindrone
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool StringCheck(string str, int start, int end)
+{
+    // base case
+    if (start > end)
+    {
+        return true;
+    }
+
+    // procedure  / updation
+    if (str[start] != str[end])
+    {
+        return false;
+    }
+
+    // Recursive call
+    else
+    {
+        StringCheck(str, start + 1, end - 1);
+    }
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << endl;
+
+    bool haiky = StringCheck(str, 0, str.length() - 1);
+
+    if (haiky)
+    {
+        cout << "It is palindrone";
+    }
+    else
+    {
+        cout << "nahi hai";
+    }
+
+    return 0;
+}
