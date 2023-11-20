446d871db8e30b30f196a16a4623a237159e9a11
Shekhar Dase <shekhardase@gmail.com>
2023-11-20 20:07:15 +0530

    new sort added

--- RecurisonD3.cpp
+++ RecurisonD3.cpp
@@ -249,3 +249,106 @@
 
 //     return 0;
 // }
+
+// next insertion sort
+//  and selection sort
+
+// selection sort
+
+// #include <iostream>
+// using namespace std;
+
+// void selectionSort(int arr[], int n)
+// {
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int minIndex = i;
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[j] < arr[minIndex])
+//             {
+//                 minIndex = j;
+//             }
+//         }
+//         swap(arr[i], arr[minIndex]);
+//     }
+// }
+
+// void printArray(int arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     int arr[] = {64, 25, 12, 22, 11};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     cout << "Original array: ";
+//     printArray(arr, n);
+
+//     selectionSort(arr, n);
+
+//     cout << "Sorted array: ";
+//     printArray(arr, n);
+
+//     return 0;
+// }
+
+// selection sort using recursion
+#include <iostream>
+using namespace std;
+
+void selectionSortRecursive(int arr[], int n, int currentIndex = 0)
+{
+    if (currentIndex == n - 1)
+    {
+        // Base case: If currentIndex reaches the last element, the array is sorted.
+        return;
+    }
+
+    // Find the index of the minimum element in the unsorted part
+    int minIndex = currentIndex;
+    for (int i = currentIndex + 1; i < n; i++)
+    {
+        if (arr[i] < arr[minIndex])
+        {
+            minIndex = i;
+        }
+    }
+
+    // Swap the found minimum element with the first unsorted element
+    swap(arr[currentIndex], arr[minIndex]);
+
+    // Recursively sort the rest of the array
+    selectionSortRecursive(arr, n, currentIndex + 1);
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    selectionSortRecursive(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
