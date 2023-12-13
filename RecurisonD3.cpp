3811a4426cf7920ba11985c11376fe73ee8d0d90
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-13 20:51:07 +0530

    Resuming practice

--- RecurisonD3.cpp
+++ RecurisonD3.cpp
@@ -420,3 +420,74 @@
 //         cout << arr[i] << " ";
 //     }
 // }
+
+// Insertion sort
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void sort(int arr[], int n)
+{
+    int start = 1;
+
+    if (n > start)
+    {
+        return;
+    }
+    int key = arr[start];
+
+    int j = start - 1;
+
+    while (j >= 0 && arr[j] > key)
+    {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+    }
+    arr[j + 1] = key;
+
+    start++;
+
+    sort(arr, n);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // insertionSort(arr, n);
+    sort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
