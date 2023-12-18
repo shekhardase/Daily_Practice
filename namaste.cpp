2e73e8edd4693eed18c031c2b39ec946f66d7693
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-18 23:25:21 +0530

    Leetcode question practice

--- namaste.cpp
+++ namaste.cpp
@@ -1,194 +1,234 @@
-// // // #include<iostream>
+// // // // #include<iostream>
+// // // // using namespace std;
+// // // // int main()
+// // // // {
+// // // //     cout<<"Namaste World";
+// // // //     return 0;
+// // // // }
+
+// // // #include <iostream>
 // // // using namespace std;
+// // // // int main()
+// // // // {
+// // // //     char a;
+// // // //     cin >> a ;
+
+// // // //     if (  a >=  'a' &&  a <= 'z')
+// // // //     {
+// // // //         cout << "It is lower case "<<endl;
+// // // //     }
+
+// // // //     else if (a >=  'A' &&  a <= 'Z')
+// // // //     {
+// // // //         cout << "It is Upper case " << endl;
+// // // //     }
+
+// // // //     else if (a>=  '0' &&  a <= '9')
+// // // //     {
+// // // //         cout<<"it is numeric "<<endl;
+// // // //     }
+
+// // // // }
+
+// // // // int main ()
+// // // // {
+// // // //     int n;
+// // // //      cin >> n;
+// // // //      int i = 1;
+
+// // // //      while(i <=n)
+// // // //      {
+
+// // // //         cout<<i<<" "; i = i+1;
+// // // //      }
+// // // // }
+
+// // // // int main()
+// // // // {
+// // // //     int n;
+// // // //     cin >>n;
+
+// // // //     int sum = 0;
+// // // //     int i = 1;
+
+// // // //     while(i<=n)
+// // // //     {
+// // // //         // cout<<sum<<endl;
+// // // //         sum= sum+i;
+// // // //         i = i+2;
+
+// // // //     }
+// // // //     cout<<sum<<endl;
+
+// // // //check number is prime or not
+
 // // // int main()
 // // // {
-// // //     cout<<"Namaste World";
-// // //     return 0;
+// // //     int n;
+// // //     cin >> n;
+
+// // //     int i = 2;
+// // //     while(i<n)
+// // //     {
+// // //         if(n%i==0)
+// // //         {
+// // //             cout<<"Not Prime"<<endl;
+// // //             break;
+// // //         // }
+// // //         i = i+1;
+// // //     }
+// // //     if(i==n)
+// // //     {
+// // //         cout<<"Prime"<<endl;
+// // //     }
 // // // }
 
+// // #include <map>
 // // #include <iostream>
 // // using namespace std;
-// // // int main()
-// // // {
-// // //     char a;
-// // //     cin >> a ;
-
-// // //     if (  a >=  'a' &&  a <= 'z')
-// // //     {
-// // //         cout << "It is lower case "<<endl;
-// // //     }
-
-// // //     else if (a >=  'A' &&  a <= 'Z')
-// // //     {
-// // //         cout << "It is Upper case " << endl;
-// // //     }
-
-// // //     else if (a>=  '0' &&  a <= '9')
-// // //     {
-// // //         cout<<"it is numeric "<<endl;
-// // //     }
-
-// // // }
-
-// // // int main ()
-// // // {
-// // //     int n;
-// // //      cin >> n;
-// // //      int i = 1;
-
-// // //      while(i <=n)
-// // //      {
-
-// // //         cout<<i<<" "; i = i+1;
-// // //      }
-// // // }
-
-// // // int main()
-// // // {
-// // //     int n;
-// // //     cin >>n;
-
-// // //     int sum = 0;
-// // //     int i = 1;
-
-// // //     while(i<=n)
-// // //     {
-// // //         // cout<<sum<<endl;
-// // //         sum= sum+i;
-// // //         i = i+2;
-
-// // //     }
-// // //     cout<<sum<<endl;
-
-// // //check number is prime or not
 
 // // int main()
 // // {
-// //     int n;
-// //     cin >> n;
+// //     map<int, int> Hagu;
 
-// //     int i = 2;
-// //     while(i<n)
+// //     Hagu[1] = 12;
+// //     Hagu[2] = 14;
+// //     Hagu[3] = 14;
+// //     Hagu[4] = 15;
+// //     Hagu[5] = 16;
+
+// //     map<int, int>::iterator Lendi;
+// //     for (Lendi = Hagu.begin(); Lendi != Hagu.end(); Lendi++)
 // //     {
-// //         if(n%i==0)
-// //         {
-// //             cout<<"Not Prime"<<endl;
-// //             break;
-// //         // }
-// //         i = i+1;
+// //         cout << (*Lendi).first << " " << (*Lendi).second << endl;
 // //     }
-// //     if(i==n)
+
+// //     // Hagu.insert({{[6] = 23}, {[7] = 123}});
+// //     Hagu[23] = 23234;
+
+// //     cout << endl
+// //          << endl;
+
+// //     map<int, int>::iterator BhagBc;
+
+// //     for (BhagBc = Hagu.begin(); BhagBc != Hagu.end(); BhagBc++)
 // //     {
-// //         cout<<"Prime"<<endl;
+// //         cout << (*BhagBc).first << " " << (*BhagBc).second << endl;
 // //     }
 // // }
 
-// #include <map>
-// #include <iostream>
-// using namespace std;
+// // #include <iostream>
+// // #include <map>
+// // using namespace std;
 
+// // int main()
+// // {
+// //     map<int, int> MyMap;
+
+// //     MyMap[1] = 12 ;
+// //     MyMap[2] = 21;
+// //     MyMap[3] = 13;
+// //     MyMap[7] = 322;
+// //     MyMap[4] = 32322;
+// //     MyMap[5] = 1232;
+// //     MyMap[6] = 12342;
+
+// //     map<int , int>:: iterator iterate;
+// //     for(iterate = MyMap.begin() ; iterate!= MyMap.end() ;iterate++)
+// //     {
+// //         // cout<<endl;
+// //         cout<<(*iterate).first <<" " <<(*iterate).second<<endl;
+
+// //     }
+
+// //    MyMap.insert(make_pair(323 , 23));
+// //    cout<<endl;
+
+// //   for(iterate = MyMap.begin() ; iterate!= MyMap.end() ;iterate++)
+// //     {
+// //         // cout<<endl;
+// //         cout<<(*iterate).first <<" " <<(*iterate).second<<endl;
+
+// //     }
+// // }
+
+// /*------18/01/2023------*/
+// #include <bits/stdc++.h> //jai ganesh
+// using namespace std;
 // int main()
 // {
-//     map<int, int> Hagu;
-
-//     Hagu[1] = 12;
-//     Hagu[2] = 14;
-//     Hagu[3] = 14;
-//     Hagu[4] = 15;
-//     Hagu[5] = 16;
-
-//     map<int, int>::iterator Lendi;
-//     for (Lendi = Hagu.begin(); Lendi != Hagu.end(); Lendi++)
+//     int t;
+//     cin >> t;
+//     while (t > 0)
 //     {
-//         cout << (*Lendi).first << " " << (*Lendi).second << endl;
+//         int n;
+//         cin >> n;
+//         int arr[n];
+//         int count = 0;
+//         int flag = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> arr[i];
+
+//             if (arr[i] == 0)
+//             {
+//                 flag = 1;
+//             }
+
+//             if (arr[i] < 0)
+//             {
+//                 count++;
+//             }
+//         }
+
+//         if (count % 2 == 0 || flag != 0)
+//             cout << "0" << endl;
+
+//         else
+//             cout << (count % 2) << endl;
+
+//         t--;
 //     }
 
-//     // Hagu.insert({{[6] = 23}, {[7] = 123}});
-//     Hagu[23] = 23234;
-
-//     cout << endl
-//          << endl;
-
-//     map<int, int>::iterator BhagBc;
-
-//     for (BhagBc = Hagu.begin(); BhagBc != Hagu.end(); BhagBc++)
-//     {
-//         cout << (*BhagBc).first << " " << (*BhagBc).second << endl;
-//     }
+//     return 0;
 // }
 
-// #include <iostream>
-// #include <map>
-// using namespace std;
-
-// int main()
-// {
-//     map<int, int> MyMap;
-
-//     MyMap[1] = 12 ;
-//     MyMap[2] = 21;
-//     MyMap[3] = 13;
-//     MyMap[7] = 322;
-//     MyMap[4] = 32322;
-//     MyMap[5] = 1232;
-//     MyMap[6] = 12342;
-
-//     map<int , int>:: iterator iterate;
-//     for(iterate = MyMap.begin() ; iterate!= MyMap.end() ;iterate++)
-//     {
-//         // cout<<endl;
-//         cout<<(*iterate).first <<" " <<(*iterate).second<<endl;
-
-//     }
-
-//    MyMap.insert(make_pair(323 , 23));
-//    cout<<endl;
-
-//   for(iterate = MyMap.begin() ; iterate!= MyMap.end() ;iterate++)
-//     {
-//         // cout<<endl;
-//         cout<<(*iterate).first <<" " <<(*iterate).second<<endl;
-
-//     }
-// }
-
-/*------18/01/2023------*/
-#include <bits/stdc++.h> //jai ganesh
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    int t;
-    cin >> t;
-    while (t > 0)
+    int n;
+    cin >> n;
+    int arr[n];
+    int store = 0;
+    int end = n;
+    int k;
+    cout << "enter k";
+    cin >> k;
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        cin >> n;
-        int arr[n];
-        int count = 0;
-        int flag = 0;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-
-            if (arr[i] == 0)
-            {
-                flag = 1;
-            }
-
-            if (arr[i] < 0)
-            {
-                count++;
-            }
-        }
-
-        if (count % 2 == 0 || flag != 0)
-            cout << "0" << endl;
-
-        else
-            cout << (count % 2) << endl;
-
-        t--;
+        cin >> arr[i];
     }
 
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        store = arr[0];
+
+        if (store < arr[i])
+        {
+            store = arr[i];
+        }
+    }
+
+    cout << store << endl;
+
+    end = arr[k];
+
+    for (int i = store; i > k; i--)
+    {
+        store = arr[i];
+    }
+
+    cout << store << endl;
 }
