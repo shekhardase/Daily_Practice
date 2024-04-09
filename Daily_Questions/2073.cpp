02416cc6dc8cb1a32f25ab3ce478006458cdcc7f
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-04-09 13:44:30 +0530

    Question Solution

--- Daily_Questions/2073.cpp
+++ Daily_Questions/2073.cpp
@@ -1,7 +1,6 @@
 
 // https://leetcode.com/problems/time-needed-to-buy-tickets/description/?envType=daily-question&envId=2024-04-09
 
-
 // class Solution {
 // public:
 //     int timeRequiredToBuy(vector<int>& tickets, int k) {
