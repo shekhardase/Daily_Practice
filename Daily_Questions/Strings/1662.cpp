e989d63348c3ce3b76b00d12ad849f14910d05d8
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-04-27 11:53:51 +0530

    New String Question

---
+++ Daily_Questions/Strings/1662.cpp
@@ -0,0 +1,22 @@
+// // https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/?envType=daily-question&envId=2024-04-27
+// class Solution
+// {
+// public:
+//     bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2)
+//     {
+//         string s1 = "";
+//         for (int i = 0; i < word1.size(); i++)
+//         {
+//             s1 = s1 + word1[i];
+//         }
+
+//         string s2 = "";
+
+//         for (int i = 0; i < word2.size(); i++)
+//         {
+//             s2 = s2 + word2[i];
+//         }
+
+//         return s1 == s2;
+//     }
+// };
