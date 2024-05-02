eb2efeefe6800c0bb878f31374e2ac759d901e9c
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-05-03 02:04:31 +0530

    Number question added

---
+++ Daily_Questions/Numbers/29.cpp
@@ -0,0 +1,19 @@
+// // https://leetcode.com/problems/divide-two-integers/description/
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int divide(int dividend, int divisor) {
+//          if (dividend == INT_MIN && divisor == -1) {
+//             return INT_MAX;
+//          }
+//          if (dividend == INT_MIN && divisor ==1){
+//             return INT_MIN;
+//          }
+//          return dividend/ divisor;
+//     }
+// };
