994f99654e347c78a710c096e835170df31bef7b
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-05-31 23:47:48 +0530

    New js question

---
+++ Daily_Questions/JavaScript/2620.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/counter/description/
+
+// /**
+//  * @param {number} n
+//  * @return {Function} counter
+//  */
+// var createCounter = function (n) {
+//     var count = -1;
+//     return function () {
+
+//         count++;
+//         return n + count;
+//     };
+// };
+
+// /**
+//  * const counter = createCounter(10)
+//  * counter() // 10
+//  * counter() // 11
+//  * counter() // 12
+//  */
