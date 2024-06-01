8eed59b247043f42b96fc7de37b94b23209d9e3a
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-01 10:46:34 +0530

    new js

---
+++ Daily_Questions/JavaScript/2621.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/sleep/
+
+// /**
+//  * @param {number} millis
+//  * @return {Promise}
+//  */
+// async function sleep(millis) {
+//     await new Promise((resolve, reject) => {
+//         setTimeout(() => resolve(millis), millis);
+//     })
+// }
+
+// /**
+//  * let t = Date.now()
+//  * sleep(100).then(() => console.log(Date.now() - t)) // 100
+//  */
