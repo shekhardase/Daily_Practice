8d79f08f1c97c8025cd500fde0608d2e26d4d184
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-10 15:55:55 +0530

    Daily question java solution

---
+++ Daily_Questions/Arrays/1051.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/height-checker/submissions/1283702565/?envType=daily-question&envId=2024-06-10
+
+// class Solution {
+// public int heightChecker(int[] heights) {
+// int[] expected = heights.clone();
+
+// Arrays.sort(expected);
+// int count = 0;
+// for (int i = 0; i < expected.length; i++) {
+// if (expected[i] != heights[i]) {
+// count++;
+// }
+// }
+// return count;
+
+// }
+// }
