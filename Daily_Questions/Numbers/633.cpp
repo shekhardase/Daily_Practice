5c52d7983de5ee8388250304b6c105d9edfcd9a7
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-17 12:01:47 +0530

    New number question cpp java daily

---
+++ Daily_Questions/Numbers/633.cpp
@@ -0,0 +1,16 @@
+// class Solution {
+// public:
+//     bool judgeSquareSum(int c) {
+//         for(long a = 0 ; a * a <=c ; a++)
+//         {
+//                 double b= sqrt(c - a * a);
+//             if(b==(int)b)
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// https://leetcode.com/problems/sum-of-square-numbers/?envType=daily-question&envId=2024-06-17
---
+++ Daily_Questions/Numbers/633.java
@@ -0,0 +1,13 @@
+// class Solution {
+
+//     public boolean judgeSquareSum(int c) {
+//         for (long a = 0; a * a <= c; a++) {
+//             double b = Math.sqrt(c - a * a);
+//             if (b == (int) b) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+// https://leetcode.com/problems/sum-of-square-numbers/?envType=daily-question&envId=2024-06-17
