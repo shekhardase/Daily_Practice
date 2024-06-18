33a257fbe6e31660040c60cf6f2099a7729d0498
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-18 11:29:06 +0530

    map_practice_RawCODES

--- RAWCODES/map.cpp
+++ RAWCODES/map.cpp
@@ -1,25 +1,53 @@
-#include <iostream>
-#include <map>
+// #include <iostream>
+// #include <map>
+// using namespace std;
+
+// int main()
+// {
+//     map<string, int> niggacount;
+
+//     niggacount["one"] = 1;
+//     niggacount["two"] = 23;
+//     niggacount["three"] = 12;
+//     niggacount["four"] = 100;
+//     niggacount["five"] = 0;
+
+//     map<string, int>::iterator countnigga;
+
+//     for (countnigga = niggacount.begin(); countnigga != niggacount.end(); countnigga++)
+//     {
+//         cout << (*countnigga).first << " " << (*countnigga).second << endl;
+//     }
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main()
 {
-    map<string, int> niggacount;
+    int n;
+    cin >> n;
+    int arr[n];
+    ; // Example initialization of 'arr'
 
-    niggacount["one"] = 1;
-    niggacount["two"] = 23;
-    niggacount["three"] = 12;
-    niggacount["four"] = 100;
-    niggacount["five"] = 0;
-
-    map<string, int>::iterator countnigga;
-
-    for (countnigga = niggacount.begin(); countnigga != niggacount.end(); countnigga++)
+    for (int i = 0; i < n; i++)
     {
-        cout << (*countnigga).first << " " << (*countnigga).second << endl;
+        cin >> arr[i];
     }
 
-    return 0;
+    map<int, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
+
+    for (auto &it : mp)
+    {
+        cout << it.first << ' '
+             << it.second << '\n';
+    }
 }
-
-
