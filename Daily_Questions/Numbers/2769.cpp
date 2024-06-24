8d7584511390d1d759901078432fc2be2442e35b
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-25 02:12:01 +0530

    Simple math number problem just need observation Easy

---
+++ Daily_Questions/Numbers/2769.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+// class Solution {
+// public:
+//     int theMaximumAchievableX(int num, int t) {
+//         int ans = num + 2 * t;
+//         return ans;
+//     }
+// };
