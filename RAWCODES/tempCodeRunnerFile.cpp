cfcecd3a95d86f6fe2177fe8c55c2e4c672ab057
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-25 23:54:26 +0530

    Binary Trees Implementation and Working

--- RAWCODES/tempCodeRunnerFile.cpp
+++ RAWCODES/tempCodeRunnerFile.cpp
@@ -1,2 +1,15 @@
+   // // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    // // level order
+    // cout << "Printing the level order tracersal output " << endl;
+    // levelOrderTraversal(root);
 
-    for (int i = 0; i < 10; i+
+    // cout << "inorder traversal is:  ";
+    // inorder(root);
+
+    // cout << endl
+    //      << "preorder traversal is:  ";
+    // preorder(root);
+
+    // cout << endl
+    //      << "postorder traversal is:  ";
+    // postorder(root);
