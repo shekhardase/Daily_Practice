54a9173690a43735437fb4988d0e63b9df5ea950
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-25 12:06:17 +0530

    New  matrix question cant solve todays daily question because of tree IDK Tree Thats why different questions

---
+++ Daily_Questions/Arrays/2160.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
+
+// class Solution {
+// public:
+//     vector<vector<int>> findMatrix(vector<int>& nums) {
+//         int n = 0;
+//         map<int,int> m ;
+
+//         for(auto x: nums)
+//         {
+//             m[x]++;
+//             n = max(n , m[x]);
+//         }
+//         vector<vector<int>> ans(n);
+
+//         vector<set<int>> s(n);
+
+//         for(auto x : nums)
+//         {
+//             for(int i = 0 ; i < n ; i++)
+//             {
+//                 if(s[i].find(x) == s[i].end())
+//                 {
+//                     ans[i].push_back(x);
+//                     s[i].insert(x);
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
