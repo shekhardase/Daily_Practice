14744bcffb85c0ae75ca43b97938fd7cc86ee2c0
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-06-26 22:10:35 +0530

    New Math problem EASY CPP

---
+++ Daily_Questions/Numbers/2469.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/convert-the-temperature/description/
+// class Solution
+// {
+//     public:
+//         vector<double> convertTemperature(double celsius)
+//         {
+
+//             vector<double> ans;
+//             double kelvin = celsius + 273.15;
+//             double fahrenheit = celsius *1.80 + 32.00;
+
+//             ans.push_back(kelvin);
+//             ans.push_back(fahrenheit);
+//             return ans;
+//         }
+// };
