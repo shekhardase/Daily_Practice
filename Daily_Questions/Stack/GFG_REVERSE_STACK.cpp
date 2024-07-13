ac327104b26b423120ff3ea07fed21b51eccff9c
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-13 13:18:40 +0530

    stack question practice

--- Daily_Questions/Stack/GFG_REVERSE_STACK.cpp
+++ Daily_Questions/Stack/GFG_REVERSE_STACK.cpp
@@ -1,6 +1,48 @@
-#include <iostream>
-#include <stack>
-using namespace std;
+// #include <iostream>
+// #include <stack>
+// using namespace std;
+
+// // void reverse(stack<int> myStack)
+// // {
+// //     if (myStack.empty())
+// //     {
+// //         return;
+// //     }
+
+// //     int top = myStack.top();
+// //     myStack.pop();
+// //     reverse(myStack);
+
+// //     stack<int> temp;
+
+// //     while (!myStack.empty())
+// //     {
+// //         temp.push(myStack.top());
+// //         myStack.pop();
+// //     }
+
+// //     myStack.push(top);
+
+// //     while (!temp.empty())
+// //     {
+// //         myStack.push(temp.top());
+// //         temp.pop();
+// //     }
+// // }
+
+// void insertAtBottom(stack<int> myStack, int element)
+// {
+//     if (myStack.empty())
+//     {
+//         myStack.push(element);
+//         return;
+//     }
+
+//     int current = myStack.top();
+//     myStack.pop();
+//     insertAtBottom(myStack, element);
+//     myStack.push(current);
+// }
 
 // void reverse(stack<int> myStack)
 // {
@@ -11,73 +53,31 @@ using namespace std;
 
 //     int top = myStack.top();
 //     myStack.pop();
-//     reverse(myStack);
 
-//     stack<int> temp;
+//     reverse(myStack);
+//     insertAtBottom(myStack, top);
+// }
+
+// int main()
+// {
+//     stack<int> myStack;
+
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         myStack.push(i);
+//     }
+
+//     cout << endl;
+//     cout << myStack.size() << endl;
+
+//     cout << endl;
+//     reverse(myStack);
 
 //     while (!myStack.empty())
 //     {
-//         temp.push(myStack.top());
+//         cout << myStack.top() << " ";
 //         myStack.pop();
 //     }
 
-//     myStack.push(top);
-
-//     while (!temp.empty())
-//     {
-//         myStack.push(temp.top());
-//         temp.pop();
-//     }
+//     return 0;
 // }
-
-void insertAtBottom(stack<int> myStack, int element)
-{
-    if (myStack.empty())
-    {
-        myStack.push(element);
-        return;
-    }
-
-    int current = myStack.top();
-    myStack.pop();
-    insertAtBottom(myStack, element);
-    myStack.push(current);
-}
-
-void reverse(stack<int> myStack)
-{
-    if (myStack.empty())
-    {
-        return;
-    }
-
-    int top = myStack.top();
-    myStack.pop();
-
-    reverse(myStack);
-    insertAtBottom(myStack, top);
-}
-
-int main()
-{
-    stack<int> myStack;
-
-    for (int i = 1; i <= 10; i++)
-    {
-        myStack.push(i);
-    }
-
-    cout << endl;
-    cout << myStack.size() << endl;
-
-    cout << endl;
-    reverse(myStack);
-
-    while (!myStack.empty())
-    {
-        cout << myStack.top() << " ";
-        myStack.pop();
-    }
-
-    return 0;
-}
