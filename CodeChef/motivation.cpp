556f98e52f3a44bf54f5011e452b2f459e9f1c63
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-17 17:23:13 +0530

    From now i will also add my codechef problems and solutions as part of my coding practice"

---
+++ CodeChef/motivation.cpp
@@ -0,0 +1,31 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     // your code goes here
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n, x;
+//         cin >> n >> x;
+
+//         vector < int > S(n), R(n);
+//         for (int i = 0; i < n; i++) {
+//             cin >> S[i] >> R[i];
+//         }
+//         int maxi = INT_MIN;
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (S[i] <= x)
+//             {
+//                 maxi = max(maxi, R[i]);
+//             }
+//         }
+
+//         cout << maxi << endl;
+
+//     }
+// }
+// https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/IMDB
