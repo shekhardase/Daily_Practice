b9e006ca4e250e57e524057a3878a2517b5413f8
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-17 22:54:46 +0530

    new question added codechef

--- CodeChef/weight_balence.cpp
+++ CodeChef/weight_balence.cpp
@@ -9,11 +9,11 @@
 //     {
 //         int w1, w2, x1, x2, m;
 //         cin >> w1 >> w2 >> x1 >> x2 >> m;
-        
-//         int diff = w2 - w1; 
+
+//         int diff = w2 - w1;
 //         int min_gain = m * x1;
 //         int max_gain = m * x2;
-        
+
 //         if (diff >= min_gain && diff <= max_gain) {
 //             cout << 1 << endl;
 //         } else {
@@ -22,6 +22,5 @@
 //     }
 //     return 0;
 // }
-
 
 // https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/WEIGHTBL
