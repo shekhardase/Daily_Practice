7b82c788c1b4f940868d862191003cebd06984a2
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-17 22:53:44 +0530

    new question added codechef

---
+++ CodeChef/weight_balence.cpp
@@ -0,0 +1,27 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int w1, w2, x1, x2, m;
+//         cin >> w1 >> w2 >> x1 >> x2 >> m;
+        
+//         int diff = w2 - w1; 
+//         int min_gain = m * x1;
+//         int max_gain = m * x2;
+        
+//         if (diff >= min_gain && diff <= max_gain) {
+//             cout << 1 << endl;
+//         } else {
+//             cout << 0 << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+
+// https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/WEIGHTBL
