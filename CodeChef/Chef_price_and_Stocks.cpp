7c127bdfe4e498498755c8a9fc8065d103cf1586
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-17 23:07:03 +0530

    another codechef question added a bit different this time

---
+++ CodeChef/Chef_price_and_Stocks.cpp
@@ -0,0 +1,30 @@
+// https : // www.codechef.com/practice/course/1to2stars/LP1TO201/problems/CSTOCK
+
+// #include <bits/stdc++.h>
+
+//         using namespace std;
+
+// int main()
+// {
+//     int T;
+//     cin >> T;
+
+//     while (T--)
+//     {
+//         double S, A, B, C;
+//         cin >> S >> A >> B >> C;
+
+//         double new_price = S + (C / 100.0) * S;
+
+//         if (new_price >= A && new_price <= B)
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//         {
+//             cout << "NO" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
