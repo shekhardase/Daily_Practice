f6d7c055e0b016772b201bde871a5ef94f2c140e
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-18 18:57:25 +0530

    new question added subarray

---
+++ CodeChef/Maximum Length Even Subarray.cpp
@@ -0,0 +1,29 @@
+// https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/MXEVNSUB
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+
+//         int sum = 0;
+//         for (int i = 1; i <= n; i++)
+//         {
+//             sum = sum + i;
+//         }
+//         if (sum % 2 == 0)
+//         {
+//             cout << n << endl;
+//         }
+//         else if (sum % 2 != 0) {
+//             cout << n - 1 << endl;
+//         }
+//     }
+
+// }
