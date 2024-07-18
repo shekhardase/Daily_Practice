9c1ff7e237f6e6a7488c4b7ee69e105cc686e204
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-18 18:39:05 +0530

    new question

---
+++ CodeChef/Turn_It.cpp
@@ -0,0 +1,27 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     // your code goes here
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int u, v, a, s;
+//         cin >> u >> v >> a >> s;
+
+//         float velocity_of_car = sqrt((u * u) - (2 * a * s));
+//         int velocity_limit = v;
+
+//         if (v < velocity_of_car)
+//         {
+//             cout << "no" << endl;
+//         }
+//         else
+//         {
+//             cout << "yes" << endl;
+//         }
+//     }
+
+// }https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/NFS?tab=statement
