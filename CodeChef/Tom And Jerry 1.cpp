8c536d4c6130100e3b7e0191ffe52de752ef2264
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-18 19:49:06 +0530

    Codechef grid question

---
+++ CodeChef/Tom And Jerry 1.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// #include <cmath>
+
+// using namespace std;
+
+// bool canReach(int a, int b, int c, int d, int K) {
+//     int distance = abs(a - c) + abs(b - d);
+//     // Check if the distance is less than or equal to K and if the difference is even
+//     return (distance <= K) && ((K - distance) % 2 == 0);
+// }
+
+// int main() {
+//     int T;
+//     cin >> T;
+//     while (T--) {
+//         int a, b, c, d, K;
+//         cin >> a >> b >> c >> d >> K;
+//         if (canReach(a, b, c, d, K)) {
+//             cout << "YES" << endl;
+//         } else {
+//             cout << "NO" << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/TANDJ1
