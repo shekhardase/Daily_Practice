67695bc18bc7a80e676d85f9b08bd28dc148f011
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-18 19:11:42 +0530

    diff question added

---
+++ CodeChef/Distinct Pair Sum.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/MANYSUMS
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     // your code goes here
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int l, r;
+//         cin >> l >> r;
+
+//         // max reachable 2r minimunm reachable 2l + 1 
+//         cout << (2 * r - 2 * l + 1) << endl;
+
+//     }
+
+// }
