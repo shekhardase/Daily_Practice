5fa69fe05a6b43b0b0fe847b45f96b9f95cad28e
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-20 20:02:51 +0530

    new question added codechef (interesting question ngl )

---
+++ CodeChef/chef loves pizza chef loves halfs.cpp
@@ -0,0 +1,24 @@
+// https://www.codechef.com/problems/CHEFIZZA
+
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int x;
+//         cin >> x;
+
+//         int equal_slice = 1;
+//         while (equal_slice * 2 <= x) {
+//             equal_slice = equal_slice * 2;
+//         }
+
+//         int smaller_slices = x - equal_slice;
+//         cout << 2 * smaller_slices << endl;
+//     }
+//     return 0;
+// }
