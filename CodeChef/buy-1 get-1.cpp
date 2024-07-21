8f4048837277265f261b2c46654558d048bbb0a3
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-21 17:47:42 +0530

    new question codechef added

---
+++ CodeChef/buy-1 get-1.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/problems/BUY1GET1?tab=statement
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     // your code goes here
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         string s;
+//         cin >> s;
+//         sort(s.begin(), s.end());
+//         int count = 0;
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == s[i + 1]) {
+//                 count++;
+//                 i++;
+//             }
+//             else
+//                 count++;
+//         }
+//         cout << count << endl;
+//     }
+// }
