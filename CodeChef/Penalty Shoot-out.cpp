fcbe26b0abd65e274cc7216776f8b6386c4cc94f
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-22 18:42:01 +0530

    new cpp question codechef

---
+++ CodeChef/Penalty Shoot-out.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/problems/PENALTYSHOOT?tab=statement
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     // your code goes here
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n, m;
+//         cin >> n >> m;
+//         if (n == m || n == m + 1 || n + 1 == m || n + 2 == m) {
+//             cout << "YES" << endl;
+//         }
+//         else cout << "NO" << endl;
+//     }
+
+// }
