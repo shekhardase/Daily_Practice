03a9e11162ac89d1b8de59753267f13f115b09e9
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-25 15:55:26 +0530

    new sorting question added

---
+++ Daily_Questions/Arrays/912.cpp
@@ -0,0 +1,28 @@
+// https : // leetcode.com/problems/sort-an-array/description/?envType=daily-question&envId=2024-07-25
+
+//         class Solution
+// {
+// public:
+//     vector<int> sortArray(vector<int> &nums)
+//     {
+//         unordered_map<int, int> mp;
+//         for (int &num : nums)
+//         {
+//             mp[num]++;
+//         }
+//         int minE = *min_element(begin(nums), end(nums));
+//         int maxE = *max_element(begin(nums), end(nums));
+
+//         int i = 0;
+//         for (int num = minE; num <= maxE; num++)
+//         {
+//             while (mp[num] > 0)
+//             {
+//                 nums[i] = num;
+//                 i++;
+//                 mp[num]--;
+//             }
+//         }
+//         return nums;
+//     }
+// };
