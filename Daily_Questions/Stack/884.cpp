16d1301e45f475721dd4d65f8b8b8775520140b5
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-26 19:15:10 +0530

    New String Question EASY HASHMAP

---
+++ Daily_Questions/Stack/884.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/uncommon-words-from-two-sentences/description/?envType=problem-list-v2&envId=puee73lv&
+
+// class Solution
+// {
+//     public:
+//         vector<string> uncommonFromSentences(string s1, string s2)
+//         {
+//             unordered_map<string, int> wordCount;
+//             vector<string> result;
+//             string word;
+
+//             stringstream ss1(s1);
+
+//             while (ss1 >> word)
+//             {
+//                 wordCount[word]++;
+//             }
+
+//             stringstream ss2(s2);
+//             while (ss2 >> word)
+//             {
+//                 wordCount[word]++;
+//             }
+
+//             for (auto i: wordCount)
+//             {
+//                 if (i.second == 1)
+//                 {
+//                     result.push_back(i.first);
+//                 }
+//             }
+//             return result;
+//         }
+// };
