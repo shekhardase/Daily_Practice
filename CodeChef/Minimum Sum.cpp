74463e735f8e15f2115e3ba35c2f6299c624f73f
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-07-30 12:54:27 +0530

    new question codechef 1300 rating reached

---
+++ CodeChef/Minimum Sum.cpp
@@ -0,0 +1,31 @@
+// https://www.codechef.com/problems/MINSM?tab=statement
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t ;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+// 	    int n ;
+// 	    cin>> n;
+
+// 	    int arr[n];
+// 	    for(int i = 0 ; i < n ; i++)
+// 	    {
+// 	        cin>>arr[i];
+// 	    }
+
+// 	    long long result = 0 ;
+// 	    for(int i = 0 ; i < n  ; i++)
+// 	    {
+// 	        result = gcd(result , arr[i]);
+// 	    }
+
+// 	    cout<<result * n <<endl;
+
+// 	}
+
+// }
