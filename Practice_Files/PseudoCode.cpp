c8a730139fb2b64fd355097fd8ae95f3315576da
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-05 12:18:12 +0530

    practice codes added

--- Practice_Files/PseudoCode.cpp
+++ Practice_Files/PseudoCode.cpp
@@ -1,35 +1,82 @@
+// // #include <iostream>
+// // using namespace std;
+
+// // int main()
+// // {
+// //     int x, y, z;
+// //     y = 1;
+// //     x = 2;
+// //     z = x ^ y;
+// //     cout << z << endl;
+// //     return 0;
+// // }
+
+// // #include <iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int n = 1;
+// //     int newn = n << 1;
+// //     int newn2 = n << 2;
+// //     int newn3 = n << 3;
+
+// //     int nc = 1;
+// //     int newnc = n >> 1;
+// //     int newn2c = n >> 2;
+// //     int newn3c = n >> 3;
+// //     cout << nc << endl;
+// //     cout << newnc << endl;
+// //     cout << newn2c << endl;
+// //     cout << newn3c << endl;
+
+// //     return 0;
+// // }
+
 // #include <iostream>
 // using namespace std;
 
 // int main()
 // {
-//     int x, y, z;
-//     y = 1;
-//     x = 2;
-//     z = x ^ y;
-//     cout << z << endl;
-//     return 0;
+//     int n = 1;
+
+//     int first = n << 1;
+//     int second = n << 2;
+//     int third = n << 3;
+//     int fourth = n << 4;
+
+//     // cout << first << endl;
+//     // cout << second << endl;
+//     // cout << third << endl;
+//     // cout << fourth << endl;
+
+//     int first1 = n << 1;
+//     int second1 = first1 << 1;
+//     int third1 = second1 << 1;
+//     int fourth1 = third1 << 1;
+
+//     cout << first1 << endl;
+//     cout << second1 << endl;
+//     cout << third1 << endl;
+//     cout << fourth1 << endl;
+// cout<<"_____________";
+//     cout <<( 3 << 1) << endl;
 // }
 
 // #include <iostream>
 // using namespace std;
+
 // int main()
 // {
-//     int n = 1;
-//     int newn = n << 1;
-//     int newn2 = n << 2;
-//     int newn3 = n << 3;
+//     char m = 'a';
+//     int count = 0;
 
-//     int nc = 1;
-//     int newnc = n >> 1;
-//     int newn2c = n >> 2;
-//     int newn3c = n >> 3;
-//     cout << nc << endl;
-//     cout << newnc << endl;
-//     cout << newn2c << endl;
-//     cout << newn3c << endl;
+//     while(m>'a')
+//     {
+//         count++;
+//         m--;
+//     }
 
-//     return 0;
+//     cout<<count;
 // }
 
 #include <iostream>
@@ -37,27 +84,8 @@ using namespace std;
 
 int main()
 {
-    int n = 1;
-
-    int first = n << 1;
-    int second = n << 2;
-    int third = n << 3;
-    int fourth = n << 4;
-
-    // cout << first << endl;
-    // cout << second << endl;
-    // cout << third << endl;
-    // cout << fourth << endl;
-
-    int first1 = n << 1;
-    int second1 = first1 << 1;
-    int third1 = second1 << 1;
-    int fourth1 = third1 << 1;
-
-    cout << first1 << endl;
-    cout << second1 << endl;
-    cout << third1 << endl;
-    cout << fourth1 << endl;
-cout<<"_____________";
-    cout <<( 3 << 1) << endl;
+    int x = 11, y = 4, z = 3;
+    y = !x;
+    z = !!x;
+    cout<<y<<" "<<z;
 }
