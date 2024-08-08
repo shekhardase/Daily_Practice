3139f39ce96449c1bf397704fbbeddd0937ae5b4
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-08 23:19:47 +0530

    new codechef question added

---
+++ CodeChef/odd repeat.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/problems/REPEAT?tab=statement#include <iostream>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t,n,k,s;cin >>t ;
+// 	while(t--) {
+// 	    cin >> n>> k >> s;
+// 	    cout << (s - (n*n))/(k-1) << endl;
+// 	}
+// 	return 0;
+// }
