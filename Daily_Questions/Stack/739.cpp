50b8a36c10a0b559c0e575bcb46df4ace395170e
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-09 23:53:41 +0530

    New leetcode question added Stack

---
+++ Daily_Questions/Stack/739.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/daily-temperatures/description/
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         stack<int> st;
+//         vector<int> ans(temperatures.size(), 0);
+//         for (int i = 0; i < temperatures.size(); i++) {
+//             while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+//                 int index = st.top();
+//                 st.pop();
+//                 ans[index] = i - index;
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+//     }
+// };
