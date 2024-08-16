d743c7ae6b329c0d81d558fffa3326917bb8ea6d
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-16 18:16:09 +0530

    Daily question leetcode arrays

---
+++ Daily_Questions/Arrays/624.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-distance-in-arrays/description/?envType=daily-question&envId=2024-08-16
+
+// class Solution {
+// public:
+//     int maxDistance(vector<vector<int>>& arrays) {
+//        int smallest = arrays[0][0];
+//        int biggest = arrays[0].back();
+
+//        int max_distance = 0 ;
+
+//        for(int i = 1; i  < arrays.size() ; i++)
+//        {
+//         max_distance = max(max_distance , abs(arrays[i].back() - smallest));
+//         max_distance = max(max_distance , abs(biggest - arrays[i][0]));
+//         smallest = min(smallest , arrays[i][0]);
+//         biggest = max(biggest , arrays[i].back());
+//        }
+//        return max_distance ;
+//     }
+// };
