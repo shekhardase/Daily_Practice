eaab689eda666710aedec83fbdeaacf1d0600c4b
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-17 15:14:46 +0530

    simple array question will optimise with binary search

---
+++ Daily_Questions/Arrays/2089.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-target-indices-after-sorting-array/description/
+
+// class Solution {
+// public:
+//     vector<int> targetIndices(vector<int>& nums, int target) {
+//        sort(nums.begin() , nums.end());
+//        vector<int>temp
+//        ;
+
+//        for(int i = 0 ; i < nums.size() ; i++)
+//        {
+//         if(nums[i] == target)
+//         {
+//             temp.push_back(i);
+//         }
+//        }
+//        return temp;
+//     }
+// };
