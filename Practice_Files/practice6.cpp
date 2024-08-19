09a3ad33eadd5fc61483e7d6a624d82dffab4d46
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-19 13:47:51 +0530

    practice codes added

--- Practice_Files/practice6.cpp
+++ Practice_Files/practice6.cpp
@@ -1,60 +1,73 @@
-#include <iostream>
-#include <vector>
+// // #include <iostream>
+// // #include <vector>
 
-using namespace std;
+// // using namespace std;
 
-void printSpiralMatrix(vector<vector<int>> &matrix)
-{
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
-    int direction = 0;
+// // void printSpiralMatrix(vector<vector<int>> &matrix)
+// // {
+// //     int rows = matrix.size();
+// //     int cols = matrix[0].size();
+// //     int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+// //     int direction = 0;
 
-    while (top <= bottom && left <= right)
-    {
-        if (direction == 0)
-        {
-            for (int i = left; i <= right; i++)
-            {
-                cout << matrix[top][i] << " ";
-            }
-            top++;
-        }
-        else if (direction == 1)
-        {
-            for (int i = top; i <= bottom; i++)
-            {
-                cout << matrix[i][right] << " ";
-            }
-            right--;
-        }
-        else if (direction == 2)
-        {
-            for (int i = right; i >= left; i--)
-            {
-                cout << matrix[bottom][i] << " ";
-            }
-            bottom--;
-        }
-        else if (direction == 3)
-        {
-            for (int i = bottom; i >= top; i--)
-            {
-                cout << matrix[i][left] << " ";
-            }
-            left++;
-        }
-        direction = (direction + 1) % 4;
-    }
-}
+// //     while (top <= bottom && left <= right)
+// //     {
+// //         if (direction == 0)
+// //         {
+// //             for (int i = left; i <= right; i++)
+// //             {
+// //                 cout << matrix[top][i] << " ";
+// //             }
+// //             top++;
+// //         }
+// //         else if (direction == 1)
+// //         {
+// //             for (int i = top; i <= bottom; i++)
+// //             {
+// //                 cout << matrix[i][right] << " ";
+// //             }
+// //             right--;
+// //         }
+// //         else if (direction == 2)
+// //         {
+// //             for (int i = right; i >= left; i--)
+// //             {
+// //                 cout << matrix[bottom][i] << " ";
+// //             }
+// //             bottom--;
+// //         }
+// //         else if (direction == 3)
+// //         {
+// //             for (int i = bottom; i >= top; i--)
+// //             {
+// //                 cout << matrix[i][left] << " ";
+// //             }
+// //             left++;
+// //         }
+// //         direction = (direction + 1) % 4;
+// //     }
+// // }
 
-int main()
-{
-    vector<vector<int>> matrix = {{1, 2, 3},
-                                  {4, 5, 6},
-                                  {7, 8, 9}};
+// // int main()
+// // {
+// //     vector<vector<int>> matrix = {{1, 2, 3},
+// //                                   {4, 5, 6},
+// //                                   {7, 8, 9}};
 
-    printSpiralMatrix(matrix);
+// //     printSpiralMatrix(matrix);
 
-    return 0;
-}
+// //     return 0;
+// // }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int i = 0;
+//     int x = i++;
+//     cout << x << endl;
+//     cout << i << endl;
+//     int y = ++i;
+//     cout << x << " " << y << endl;
+//     return 0;
+// }
