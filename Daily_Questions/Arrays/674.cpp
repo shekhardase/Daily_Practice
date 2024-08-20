49ed23a4175610652a7d4a9ee687404931812615
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-20 23:42:48 +0530

    code update

--- Daily_Questions/Arrays/674.cpp
+++ Daily_Questions/Arrays/674.cpp
@@ -1,32 +1,32 @@
-https : // leetcode.com/problems/longest-continuous-increasing-subsequence/description/
+// https : // leetcode.com/problems/longest-continuous-increasing-subsequence/description/
 
-        class Solution
-{
-public:
-    int findLengthOfLCIS(vector<int> &nums)
-    {
-        if (nums.empty())
-            return 0;
+//         class Solution
+// {
+// public:
+//     int findLengthOfLCIS(vector<int> &nums)
+//     {
+//         if (nums.empty())
+//             return 0;
 
-        int n = nums.size();
-        int count = 1;
-        int maxi = 1;
+//         int n = nums.size();
+//         int count = 1;
+//         int maxi = 1;
 
-        for (int i = 1; i < n; i++)
-        {
-            if (nums[i] > nums[i - 1])
-            {
-                count++;
-            }
-            else
-            {
-                maxi = max(count, maxi);
-                count = 1;
-            }
-        }
-        // Update maxi for the last increasing subsequence
-        maxi = max(count, maxi);
+//         for (int i = 1; i < n; i++)
+//         {
+//             if (nums[i] > nums[i - 1])
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 maxi = max(count, maxi);
+//                 count = 1;
+//             }
+//         }
+//         // Update maxi for the last increasing subsequence
+//         maxi = max(count, maxi);
 
-        return maxi;
-    }
-};
+//         return maxi;
+//     }
+// };
