d2472434a20ab590289359fe9cddedd9109b4da1
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-21 19:41:45 +0530

    Search in rotated sorted array questino added binary search

---
+++ Daily_Questions/Arrays/33.cpp
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1363537280/
+
+// class Solution
+// {
+//     public:
+//         int search(vector<int> &nums, int target)
+//         {
+//             int low = 0;
+//             int high = nums.size() - 1;
+
+//             while (low <= high)
+//             {
+//                 int mid = low + (high - low) / 2;
+
+//                 if (nums[mid] == target)
+//                 {
+//                     return mid;
+//                 }
+//                 if (nums[low] <= nums[mid])
+//                 {
+//                     if (nums[low] <= target && target <= nums[mid])
+//                     {
+//                         high = mid - 1;
+//                     }
+//                     else
+//                     {
+//                         low = mid + 1;
+//                     }
+//                 }
+//                 else
+//                 {
+//                     if (nums[mid] <= target && target <= nums[high])
+//                     {
+//                         low = mid + 1;
+//                     }
+//                     else
+//                     {
+//                         high = mid - 1;
+//                     }
+//                 }
+//             }
+//             return -1;
+//         }
+// };
