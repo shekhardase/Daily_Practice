f42cb3ff5dd1877949309a9f47da27e2210c9903
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-22 12:52:01 +0530

    code update alterate code

--- Daily_Questions/Arrays/153.cpp
+++ Daily_Questions/Arrays/153.cpp
@@ -1,36 +1,56 @@
-// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/1364367889/
-// class Solution
-// {
-//     public:
-//         int findMin(vector<int> &nums)
+// // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/1364367889/
+// // class Solution
+// // {
+// //     public:
+// //         int findMin(vector<int> &nums)
+// //         {
+// //             int mini = INT_MAX;
+
+// //             int start = 0;
+// //             int end = nums.size() - 1;
+
+// //             while (start <= end)
+// //             {
+// //                 int mid = start + (end - start) / 2;
+
+// //                	// If the left side is sorted
+// //                 if (nums[start] <= nums[mid])
+// //                 {
+// //                    	// Update the minimum with the start element
+// //                     mini = min(mini, nums[start]);
+// //                    	// Move to the right part
+// //                     start = mid + 1;
+// //                 }
+// //                 else
+// //                 {
+// //                    	// If the right side is sorted or pivot point is in the left part
+// //                    	// Update the minimum with the mid element
+// //                     mini = min(mini, nums[mid]);
+// //                    	// Move to the left part
+// //                     end = mid - 1;
+// //                 }
+// //             }
+
+// //             return mini;
+// //         }
+// // };
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int start = 0 ;
+//         int end = nums.size()-1;
+//         while(start< end)
 //         {
-//             int mini = INT_MAX;
-
-//             int start = 0;
-//             int end = nums.size() - 1;
-
-//             while (start <= end)
+//             int mid = start+(end-start)/2;
+//             if(nums[end]<nums[mid])
 //             {
-//                 int mid = start + (end - start) / 2;
-
-//                	// If the left side is sorted
-//                 if (nums[start] <= nums[mid])
-//                 {
-//                    	// Update the minimum with the start element
-//                     mini = min(mini, nums[start]);
-//                    	// Move to the right part
-//                     start = mid + 1;
-//                 }
-//                 else
-//                 {
-//                    	// If the right side is sorted or pivot point is in the left part
-//                    	// Update the minimum with the mid element
-//                     mini = min(mini, nums[mid]);
-//                    	// Move to the left part
-//                     end = mid - 1;
-//                 }
+//                 start=mid+1;
 //             }
-
-//             return mini;
+//             else{
+//                 end = mid;
+//             }
 //         }
+//         return nums[start];
+
+//     }
 // };
