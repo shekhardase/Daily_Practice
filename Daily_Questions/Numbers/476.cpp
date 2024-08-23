4bcdc3eeebeba8c00ebb07a59c1043380f092c61
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-23 12:35:15 +0530

    code update yesterdays daily question

--- Daily_Questions/Numbers/476.cpp
+++ Daily_Questions/Numbers/476.cpp
@@ -1,24 +1,60 @@
-// https://leetcode.com/problems/number-complement/description/?envType=daily-question&envId=2024-08-22
+// // https://leetcode.com/problems/number-complement/description/?envType=daily-question&envId=2024-08-22
+
+// // class Solution
+// // {
+// //     public:
+// //         int findComplement(int num)
+// //         {
+// //            	// Edge case for 0, since the complement of 0 in binary (which is 1) is simply 1
+// //             if (num == 0) return 1;
+
+// //            	// Create a mask with all bits set to 1 that is as long as the binary representation of num
+// //             int mask = 0;
+// //             int temp = num;
+
+// //             while (temp > 0)
+// //             {
+// //                 mask = (mask << 1) | 1;
+// //                 temp >>= 1;
+// //             }
+
+// //            	// XOR num with mask to get the complement
+// //             return num ^ mask;
+// //         }
+// // };
 
 // class Solution
 // {
 //     public:
 //         int findComplement(int num)
 //         {
-//            	// Edge case for 0, since the complement of 0 in binary (which is 1) is simply 1
-//             if (num == 0) return 1;
+//             string form = "";
 
-//            	// Create a mask with all bits set to 1 that is as long as the binary representation of num
-//             int mask = 0;
-//             int temp = num;
-
-//             while (temp > 0)
+//             while (num)
 //             {
-//                 mask = (mask << 1) | 1;
-//                 temp >>= 1;
+//                 int temp = num % 2;
+//                 num = num / 2;
+//                 if (temp == 1)
+//                 {
+//                     form += '1';
+//                 }
+//                 else
+//                 {
+//                     form += '0';
+//                 }
 //             }
+//             reverse(form.begin(), form.end());
+//             cout << form;
+//             int ans = 0;
+//             for (int i = form.size() - 1; i >= 0; i--)
+//             {
+//                 if (form[i] == '0')
+//                 {
 
-//            	// XOR num with mask to get the complement
-//             return num ^ mask;
+//                     int t = form.size() - i - 1;
+//                     ans += pow(2, t);
+//                 }
+//             }
+//             return ans;
 //         }
 // };
