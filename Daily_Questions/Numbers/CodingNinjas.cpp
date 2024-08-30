615bcdb2bb345fd8497aab2ad20b4dfa20cc56e5
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-08-30 22:12:56 +0530

    new question numbers binary search coding ninjas

---
+++ Daily_Questions/Numbers/CodingNinjas.cpp
@@ -0,0 +1,35 @@
+// https://www.naukri.com/code360/problems/nth-root-of-m_1062679?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries&leftPanelTabValue=PROBLEM
+// //return 1 if == m
+// // return 0 if < m
+// // return 2 if > m
+
+// int func(int mid, int n , int m)
+// {
+//   long long ans = 1;
+//   for(int i = 1 ; i <= n ; i++)
+//   {
+//     ans = ans * mid;
+//     if(ans>m) return 2;
+//   }
+//   if(ans==m) return 1;
+//   return 0;
+// }
+
+
+
+// int NthRoot(int n, int m) {
+//   // Write your code here.
+//   int low = 1 , high = m;
+//   while(low<=high)
+//   {
+//     int mid= (low+high)/2;
+//     int midN = func(mid, n , m );
+//     if(midN == 1)
+//     {
+//       return mid;
+//     }
+//     else if(midN==0) low = mid+1;
+//     else high = mid-1;
+//   }
+//   return -1;
+// }
