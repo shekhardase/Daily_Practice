5f9eefe6237ddbaee385cb739727a18774f04797
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-10-12 19:25:57 +0530

    Kth element in two sorted arrays Paratially submitted Need optimal Solution

---
+++ Coding Ninjas/kth_element)in_two_Sorted_arrays.cpp
@@ -0,0 +1,19 @@
+// https://www.naukri.com/code360/problems/k-th-element-of-2-sorted-array_1164159?leftPanelTabValue=SUBMISSION
+
+// #include <bits/stdc++.h>
+// int kthElement(vector<int> &arr1, vector<int> &arr2, int n, int m, int k) {
+//   // Write your code here
+//   vector<int> temp;
+
+//   for (int i = 0; i < n; i++) {
+//     temp.push_back(arr1[i]);
+//   }
+
+//   for (int i = 0; i < m; i++) {
+//     temp.push_back(arr2[i]);
+//   }
+
+//   sort(temp.begin() ,temp.end());
+
+//   return temp[k-1];
+// }
