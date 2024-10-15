e0053d9f26258fee762f49e1b998c7c453653cf5
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-10-15 22:24:57 +0530

    Code update

--- Daily_Questions/Heap/1962.cpp
+++ Daily_Questions/Heap/1962.cpp
@@ -1,28 +1,28 @@
-https : // leetcode.com/problems/remove-stones-to-minimize-the-total/description/
-        class Solution
-{
-public:
-    int minStoneSum(vector<int> &piles, int k)
-    {
-        priority_queue<int> pq;
-        for (int i = 0; i < piles.size(); i++)
-        {
-            pq.push(piles[i]);
-        }
+// https : // leetcode.com/problems/remove-stones-to-minimize-the-total/description/
+//         class Solution
+// {
+// public:
+//     int minStoneSum(vector<int> &piles, int k)
+//     {
+//         priority_queue<int> pq;
+//         for (int i = 0; i < piles.size(); i++)
+//         {
+//             pq.push(piles[i]);
+//         }
 
-        while (k--)
-        {
-            int ele = pq.top();
-            pq.pop();
-            pq.push(ele - (ele / 2));
-        }
+//         while (k--)
+//         {
+//             int ele = pq.top();
+//             pq.pop();
+//             pq.push(ele - (ele / 2));
+//         }
 
-        int newcount = 0;
-        while (!pq.empty())
-        {
-            newcount += pq.top();
-            pq.pop();
-        }
-        return newcount;
-    }
-};
+//         int newcount = 0;
+//         while (!pq.empty())
+//         {
+//             newcount += pq.top();
+//             pq.pop();
+//         }
+//         return newcount;
+//     }
+// };
