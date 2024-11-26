61532dae673f3ba23615dcee0138495dfce62549
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-11-26 14:03:50 +0530

    Code update

--- CodeChef/buy-1 get-1.cpp
+++ CodeChef/buy-1 get-1.cpp
@@ -4,17 +4,21 @@
 
 // using namespace std;
 
-// int main() {
+// int main()
+// {
 //     // your code goes here
 //     int t;
 //     cin >> t;
-//     while (t--) {
+//     while (t--)
+//     {
 //         string s;
 //         cin >> s;
 //         sort(s.begin(), s.end());
 //         int count = 0;
-//         for (int i = 0; i < s.size(); i++) {
-//             if (s[i] == s[i + 1]) {
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             if (s[i] == s[i + 1])
+//             {
 //                 count++;
 //                 i++;
 //             }
