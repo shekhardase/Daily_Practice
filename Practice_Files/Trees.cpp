43da194da74d6cdc435f34029be6f2b84847e1d9
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-12-19 13:25:52 +0530

    Trees Files and BSF

--- Practice_Files/Trees.cpp
+++ Practice_Files/Trees.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class node
 {
+
 public:
     int data;
     node *left;
@@ -16,12 +16,11 @@ public:
     }
 };
 
-node *BuildTree(node *root)
+node *build(node *root)
 {
     cout << "Enter the data : " << endl;
     int data;
     cin >> data;
-
     root = new node(data);
 
     if (data == -1)
@@ -29,60 +28,16 @@ node *BuildTree(node *root)
         return NULL;
     }
 
-    cout << "Enter data for inserting in left : " << data << endl;
-
-    root->left = BuildTree(root->left);
-    cout << "Enter data for inserting in Right : " << data << endl;
-
-    root->right = BuildTree(root->right);
-
+    cout << "Enter Data for Left Node : " << data << endl;
+    root->left = build(root->left);
+    cout << "Enter Data for Right Node : " << data << endl;
+    root->right = build(root->right);
     return root;
-}
-
-void LevelOrderTraversal(node *root) // also called as Breadth-First Search (BFS) traversal
-{
-    queue<node *> q;
-    q.push(root);
-    q.push(NULL);
-
-    while (!q.empty())
-    {
-        node *temp = q.front();
-
-        q.pop();
-
-        if (temp == NULL)
-        {
-            // purana level complete traverse ho chuka hai
-            cout << endl;
-            if (!q.empty()) // q still has some child nodes
-            {
-                q.push(NULL);
-            }
-        }
-        else
-        {
-            cout << " " <<temp->data << " ";
-            if (temp->l1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1eft)
-            {
-                q.push(temp->left);
-            }
-
-            if (temp->right)
-            {
-                q.push(temp->right);
-            }
-        }
-    }
 }
 
 int main()
 {
     node *root = NULL;
-    root = BuildTree(root);
-    cout << "Printing the level order traversal output " << endl;
-    LevelOrderTraversal(root);
+    build(root);
     return 0;
 }
-
-// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
