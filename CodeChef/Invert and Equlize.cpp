8c969e81a78c686702ad2791fc8ddde0c132598c
Shekhar <115777486+shekhardase@users.noreply.github.com>
2025-01-06 18:20:32 +0530

    new Codechef question added

---
+++ CodeChef/Invert and Equlize.cpp
@@ -0,0 +1,32 @@
+// https://www.codechef.com/problems/INVEQ?tab=statement
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+// 	    int n;
+// 	    cin>> n ;
+// 	    string s;
+// 	    cin>>s;
+	    
+
+//     int count = 0;
+//     for(int i = 0 ; i < n ;i++)
+//     {
+//         if(s[i]!=s[i+1])
+//         {
+//             count++;
+//         }
+//     }
+	    
+// 	    cout<<count/2<<endl;
+// 	   // cout<<contizero << " "<<contione<<endl;
+	    
+// 	}
+
+// }
+// a/
