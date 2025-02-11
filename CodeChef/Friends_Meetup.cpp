d4e1496bdf1df9f4dd724cae159a7399837d33b3
Shekhar <115777486+shekhardase@users.noreply.github.com>
2025-02-11 12:05:25 +0530

    Codechef Daily Questiona dded

---
+++ CodeChef/Friends_Meetup.cpp
@@ -0,0 +1,33 @@
+// https://www.codechef.com/problems/FRIMEET
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+// 	    int x1 , x2;
+// 	    cin>>x1>>x2;
+	    
+// 	    if(x1 == x2)
+// 	    {
+// 	        cout<<"YES"<<endl;
+// 	        continue;
+// 	    }
+	    
+// 	   if(x1 < x2)
+// 	   {
+// 	       cout<<"NO"<<endl;
+// 	       continue;
+// 	   }
+	   
+// 	   else{
+// 	       cout<<"YES"<<endl;
+// 	       continue;
+// 	   }
+// 	}
+
+// }
